a4a706a23055694e535767c4a88ed7faaef92c56
Vander Lima <vandercbl@gmail.com>
2022-04-04 19:25:09 -0300

    Adicionando solução de limpeza de cache

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { ThemeProvider } from 'styled-components'
 import GlobalStyle from './styles/global'
 import Theme from './styles/theme'
 import MainRoutes from './routes'
+import packageJson from '../package.json'
+
+global.appVersion = packageJson.version
 
 function App() {
 	return (
--- src/pages/DivulgationCenter/index.jsx
+++ src/pages/DivulgationCenter/index.jsx
@@ -1,27 +1,61 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 function DivulgationCenter() {
 	const navigate = useNavigate()
-	// const urlRedirect = 'https://www.instagram.com/juventudemens/'
-	const urlRedirect = 'https://www.google.com'
+	const urlRedirect = 'https://www.instagram.com/juventudemens/'
 
 	const redirectPage = (url) => {
 		// eslint-disable-next-line prefer-regex-literals
 		const regexp = new RegExp(/^http/i)
 		const hasHttp = regexp.test(url)
 		if (hasHttp) {
-			window.location = url
+			setTimeout(() => {
+				window.location = url
+			}, 3000)
 		} else {
 			navigate(url)
 		}
 	}
 
+	const [desatualizado, setDesatualizado] = useState(false)
+
 	useEffect(() => {
-		redirectPage(urlRedirect)
+		fetch(`/meta.json?${new Date().getTime()}`, { cache: 'no-cache' })
+			// headers: {
+			// 	'Content-Type': 'application/json',
+			// 	Accept: 'application/json',
+			// },
+			// })
+			.then((response) => response.json())
+			.then(async (meta) => {
+				const latestVersion = meta.version
+				const currentVersion = global.appVersion
+				console.log('latestVersion', latestVersion)
+				console.log('currentVersion', currentVersion)
+				if (latestVersion !== currentVersion) {
+					console.log('nova versão')
+					setDesatualizado(true)
+					if (caches) {
+						const names = await caches.keys()
+						console.log(names)
+						await Promise.all(names.map((name) => caches.delete(name)))
+					}
+					window.location.reload()
+				} else {
+					console.log('versão velha')
+					setDesatualizado(false)
+					redirectPage(urlRedirect)
+				}
+			})
 	}, [])
 
-	return <p>Carregando...</p>
+	return (
+		<>
+			{desatualizado ? <p>Desatualizado</p> : <p>Carregando...</p>}
+			<p>~</p>
+		</>
+	)
 }
 
 export default DivulgationCenter
