c795f98540c4126f05cc47c21674dd859f8793d6
Vander Lima <vandercbl@gmail.com>
2022-04-04 15:27:01 -0300

    Teste de cache

--- src/pages/DivulgationCenter/index.jsx
+++ src/pages/DivulgationCenter/index.jsx
@@ -3,7 +3,8 @@ import { useNavigate } from 'react-router-dom'
 
 function DivulgationCenter() {
 	const navigate = useNavigate()
-	const urlRedirect = 'https://www.instagram.com/juventudemens/'
+	// const urlRedirect = 'https://www.instagram.com/juventudemens/'
+	const urlRedirect = 'https://www.google.com'
 
 	const redirectPage = (url) => {
 		// eslint-disable-next-line prefer-regex-literals
